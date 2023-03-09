3eb562a67a9df86df70cc2a0c381f4495dc982d5
Chris Hawkins <me@chrishawkins.us>
2023-03-09 09:36:26 -0500

    initial commit

--- src/lib/siteConfig.js
+++ src/lib/siteConfig.js
@@ -1,13 +1,13 @@
 export const SITE_URL = 'https://swyxkit.netlify.app';
-export const APPROVED_POSTERS_GH_USERNAME = ['sw-yx'];
-export const GH_USER_REPO = 'sw-yx/swyxkit'; // used for pulling github issues and offering comments
-export const SITE_TITLE = 'SwyxKit';
-export const SITE_DESCRIPTION = "swyx's default SvelteKit + Tailwind starter";
+export const APPROVED_POSTERS_GH_USERNAME = ['chriswhawkins'];
+export const GH_USER_REPO = 'chriswhawkins/github-issues-ds'; // used for pulling github issues and offering comments
+export const SITE_TITLE = 'Design System TestKit';
+export const SITE_DESCRIPTION = "Design system powered by Github Issues";
 export const DEFAULT_OG_IMAGE =
 	'https://user-images.githubusercontent.com/6764957/147861359-3ad9438f-41d1-47c8-aa05-95c7d18497f0.png';
-export const MY_TWITTER_HANDLE = 'swyx';
-export const MY_YOUTUBE = 'https://youtube.com/swyxTV';
-export const POST_CATEGORIES = ['Essay', 'Note']; // Other categories you can consider adding: Talks, Tutorials, Snippets, Podcasts, Notes...
+export const MY_TWITTER_HANDLE = 'chriswhawkins';
+export const MY_YOUTUBE = 'https://youtube.com/chriswhawkins';
+export const POST_CATEGORIES = ['Token', 'Element', 'Component', 'View']; // Other categories you can consider adding: Talks, Tutorials, Snippets, Podcasts, Notes...
 export const GH_PUBLISHED_TAGS = ['Published'];
 
 // auto generated variables
